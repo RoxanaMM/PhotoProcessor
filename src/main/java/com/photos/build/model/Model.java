c79fe1ca547f59541735ded167ae61f2acee203a
RoxanaMM <roxanam.mihaila@live.com>
2018-06-07 10:22:27 +0300

    functioneaza as should + refactor

--- src/main/java/com/photos/build/model/Model.java
+++ src/main/java/com/photos/build/model/Model.java
@@ -1,7 +1,55 @@
 package com.photos.build.model;
 
+import com.photos.enums.AlgoName;
+import com.photos.enums.AlgorithmConstants;
+import com.photos.enums.TypesOfSet;
 import com.photos.main.MainClass;
 
-public class Model extends MainClass{
+import java.util.HashMap;
+import java.util.Map;
 
+public class Model{
+
+    static Map<AlgoName, double[]> algoPowerModel = new HashMap<AlgoName, double[]>();
+    static Map<Integer,AlgoName>algoNameMap = new HashMap<Integer, AlgoName>();
+    static Map<Integer,TypesOfSet>typeOfSet = new HashMap<Integer, TypesOfSet>();
+
+    public Model(Map<AlgoName, double[]> algoPowerModel){
+        this.algoPowerModel = algoPowerModel;
+    }
+    public Model(){
+
+    }
+
+    public static void buildNameMaps() {
+        int z = 0;
+        for (AlgoName algoName : AlgoName.values()) {
+            algoNameMap.put(z, algoName);
+            z++;
+        }
+
+        int t = 0;
+        for (TypesOfSet type : TypesOfSet.values()) {
+            typeOfSet.put(t, type);
+            t++;
+        }
+    }
+
+    public static Map<AlgoName, double[]> buildModel(double[][] calculateTheDistance, int length){
+        buildNameMaps();
+        int p=0;
+        double [][]processed = new double[AlgorithmConstants.NR_OF_ALGORITHMS][length];
+        for(int h=0;h<AlgorithmConstants.NR_OF_ALGORITHMS;h++){
+            for(int l=0;l<length;l++){
+                if(h==p) {
+                    processed[h][l] = calculateTheDistance[l][h];
+                }
+            }
+            p++;
+        }
+        for(int k=0;k<AlgorithmConstants.NR_OF_ALGORITHMS;k++){
+            algoPowerModel.put(algoNameMap.get(k),processed[k]);
+        }
+        return algoPowerModel;
+    }
 }
--- src/main/java/com/photos/main/MainClass.java
+++ src/main/java/com/photos/main/MainClass.java
@@ -11,10 +11,7 @@ import com.photos.processing.GrayScale;
 import org.opencv.core.Core;
 
 import java.io.File;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MainClass extends Calculate {
@@ -24,20 +21,9 @@ public class MainClass extends Calculate {
     }
 
     static int i = 0;
-    // hue varies from 0 to 179, see cvtColor
-    static float[] hranges = {0, 180};
-    // saturation varies from 0 (black-gray-white) to
-    // 255 (pure spectrum color)
-    static float[] sranges = {0, 256};
-    static float[][] ranges = {hranges, sranges};
-    public static final int MAX_PIXELS = 256;
-    private float[][] firstPicPdf;
-    static Map<AlgoName, double[]> algoPowerModel = new HashMap<AlgoName, double[]>();
-    static Map<Integer,AlgoName>algoNameMap = new HashMap<Integer, AlgoName>();
-    static Map<Integer,TypesOfSet>typeOfSet = new HashMap<Integer, TypesOfSet>();
-
     static Model modelHelper = new Model();
-    static List<Model> m = new ArrayList<Model>(51);
+    static Map<AlgoName, double[]> algoPowerModel = new HashMap<AlgoName, double[]>();
+    static Map<TypesOfSet,Object > power = new HashMap<TypesOfSet,Object >();
     private FilteredSepia filteredSepia;
 
     public static void compareSimilarityVector(double[]similarities){
@@ -75,36 +61,16 @@ public class MainClass extends Calculate {
         for (int i = 0; i < listOfFiles.length-1; i++) {
             calculateTheDistance[i] = calculateDistance(greyscleValues[i], greyscleValues[i + 1], TypesOfSet.Greyscale);
         }
-        int z=0;
-        for(AlgoName algoName:AlgoName.values()){
-            algoNameMap.put(z,algoName);
-            z++;
-        }
-
-        int t=0;
-        for(TypesOfSet type : TypesOfSet.values()){
-            typeOfSet.put(t,type);
-            t++;
-        }
 
 
 
-        int p=0;
-        double [][]processed = new double[AlgorithmConstants.NR_OF_ALGORITHMS][listOfFiles.length];
-        for(int h=0;h<AlgorithmConstants.NR_OF_ALGORITHMS;h++){
-            for(int l=0;l<listOfFiles.length;l++){
-                if(h==p) {
-                    processed[h][l] = calculateTheDistance[l][h];
-                }
-            }
-            p++;
-        }
-        double [][]filler = new double[1][1];
-        for(int k=0;k<AlgorithmConstants.NR_OF_ALGORITHMS;k++){
-         algoPowerModel.put(algoNameMap.get(k),processed[k]);
-        }
 
-                calculateTheDistance = new double[listOfFiles.length][AlgorithmConstants.NR_OF_ALGORITHMS];
+       // double [][]processed = new double[AlgorithmConstants.NR_OF_ALGORITHMS][listOfFiles.length];
+
+        algoPowerModel = modelHelper.buildModel(calculateTheDistance,listOfFiles.length);
+        power.put(TypesOfSet.Greyscale, algoPowerModel);
+
+        calculateTheDistance = new double[listOfFiles.length][AlgorithmConstants.NR_OF_ALGORITHMS];
         float[][][] colouredPdfs = coloured.calculateAndDrawHistogram(folder);
         for (int i = 0; i < listOfFiles.length - 1; i++) {
             calculateTheDistance[i] = calculateDistance(colouredPdfs[i][0], colouredPdfs[i + 1][0], TypesOfSet.Coloured);
